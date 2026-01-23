2b8cd45dbdc0761d5757ed28c0a33b785f108258
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2026-01-23 22:58:09 +0300

    Update MPesaService.java

--- src/main/java/com/MPesaStkPush/MPesaStkPush/service/MPesaService.java
+++ src/main/java/com/MPesaStkPush/MPesaStkPush/service/MPesaService.java
@@ -2,6 +2,7 @@ package com.MPesaStkPush.MPesaStkPush.service;
 
 import kong.unirest.json.JSONException;
 
+//Interface
 public interface MPesaService {
     String initiateStkPush(String phoneNumber, Float amount) throws JSONException;
     void handleCallback(String callbackPayload) throws JSONException;
